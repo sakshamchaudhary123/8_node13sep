51c3bd84ec46075a29f97d1060e49fa46dd4f5b6
Saksham Chaudhary <chaudharysaksham123@gmail.com>
2024-09-13 16:43:47 +0530

    Second commit

--- routes/user.js
+++ routes/user.js
@@ -7,4 +7,8 @@ router.get('/users', function(req, res){
     usercontroller.getAllUsers(req, res);
 }) 
 
+router.get('/user/:id', function(req, res){
+    usercontroller.getUser(req, res);
+})
+
 module.exports = router;
